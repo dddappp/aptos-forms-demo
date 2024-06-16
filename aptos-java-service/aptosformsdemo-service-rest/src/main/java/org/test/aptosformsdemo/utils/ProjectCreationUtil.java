80ea905b1f8dc914f6b323fa0a4133fe2bd4d7f8
yangjiefeng <yangjiefeng@gmail.com>
2024-06-16 21:12:33 +0800

    add utils classes

---
+++ aptos-java-service/aptosformsdemo-service-rest/src/main/java/org/test/aptosformsdemo/utils/ProjectCreationUtil.java
@@ -0,0 +1,138 @@
+package org.test.aptosformsdemo.utils;
+
+import okhttp3.*;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Sink;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Objects;
+
+public class ProjectCreationUtil {
+    public static final String FORM_SCHEMA_FILES_FIELD_NAME = "files";
+    public static final String DEFAULT_GENERATED_ZIP_NAME = "Generated.zip";
+
+    public static void createContractProject(
+            OkHttpClient client,
+            String serviceUrl,
+            File[] formSchemaFiles,
+            String aptosPackageName,
+            Map<String, String> creationOptions,
+            String saveDirectory
+    ) throws IOException {
+        creationOptions.put("boundedContextAptosPackageName", aptosPackageName);
+        fixProjectCreationOptions(creationOptions);
+
+        MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        for (File file : formSchemaFiles) {
+            builder.addFormDataPart(FORM_SCHEMA_FILES_FIELD_NAME, file.getName(),
+                    RequestBody.create(file, MediaType.parse("application/octet-stream")));
+        }
+        for (Map.Entry<String, String> entry : creationOptions.entrySet()) {
+            builder.addFormDataPart(entry.getKey(), entry.getValue());
+        }
+        // Build the request body
+        RequestBody requestBody = builder.build();
+        // Build the request
+        Request request = new Request.Builder()
+                .url(serviceUrl)
+                .post(requestBody)
+                .build();
+        // Send the request
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected HTTP code: " + response);
+            File saveFile = getSaveFile(saveDirectory, response);
+            try (Sink sink = Okio.sink(saveFile);
+                 BufferedSink bufferedSink = Okio.buffer(sink)) {
+                bufferedSink.writeAll(Objects.requireNonNull(response.body()).source());
+            }
+        }
+    }
+
+    private static void fixProjectCreationOptions(Map<String, String> creationOptions) {
+        // Fix the creationOptions, if necessary.
+        String aptosPackageName = creationOptions.get("boundedContextAptosPackageName");
+        if (!creationOptions.containsKey("boundedContextName")) {
+            creationOptions.put("boundedContextName", "Dddappp." + aptosPackageName);
+        }
+        if (!creationOptions.containsKey("aptosMoveProjectDirectoryPath")) {
+            creationOptions.put("aptosMoveProjectDirectoryPath", ".");
+        }
+        if (!creationOptions.containsKey("boundedContextAptosNamedAddress")) {
+            // to snake_case
+            String aptosNamedAddress = aptosPackageName.replaceAll("([a-z0-9])([A-Z])", "$1_$2").toLowerCase();
+            creationOptions.put("boundedContextAptosNamedAddress", aptosNamedAddress);
+        }
+        if (!creationOptions.containsKey("xRenderFormUpdatable")) {
+            creationOptions.put("xRenderFormUpdatable", "true");
+        }
+        if (!creationOptions.containsKey("xRenderFormStartPageName")) {
+            creationOptions.put("xRenderFormStartPageName", "StartPage");
+        }
+        if (!creationOptions.containsKey("pomGroupId")) {
+            creationOptions.put("pomGroupId", "dddappp." + aptosPackageName.toLowerCase());
+        }
+        if (!creationOptions.containsKey("xRenderFormId")) {
+            creationOptions.put("xRenderFormId", aptosPackageName);
+        }
+        //creationOptions.put("xRenderFormOpenAt", "2023-11-06T07:36:54Z");
+        //creationOptions.put("xRenderFormCutoffAt", "2093-11-08T07:36:54+08:00");
+        if (!creationOptions.containsKey("noJavaProjects")) {
+            creationOptions.put("noJavaProjects", "true");
+        }
+        if (!creationOptions.containsKey("boundedContextJavaPackageName")) {
+            creationOptions.put("boundedContextJavaPackageName", "org.dddml." + aptosPackageName.toLowerCase());
+        }
+        if (!creationOptions.containsKey("javaProjectNamePrefix")) {
+            creationOptions.put("javaProjectNamePrefix", aptosPackageName.toLowerCase());
+        }
+        if (!creationOptions.containsKey("javaProjectsDirectoryPath")) {
+            creationOptions.put("javaProjectsDirectoryPath", "aptos-java-service");
+        }
+    }
+
+    private static File getSaveFile(String saveDirectory, Response response) {
+        String contentDisposition = response.header("Content-Disposition");
+        String saveFileName = DEFAULT_GENERATED_ZIP_NAME;
+        if (contentDisposition != null && contentDisposition.contains("filename=")) {
+            // System.out.println("contentDisposition: " + contentDisposition);
+            // for example: attachment; filename=Generated.zip; filename*=UTF-8''Generated.zip
+            String[] parts = contentDisposition.split(";");
+            for (String part : parts) {
+                if (part.trim().startsWith("filename=")) {
+                    saveFileName = part.substring(part.indexOf("filename=") + 9).replace("\"", "");
+                    break;
+                }
+            }
+        }
+        String saveFilePath = Paths.get(saveDirectory, saveFileName).toString();
+        // Check if the file exists, if it does, change the file name
+        File saveFile = new File(saveFilePath);
+        if (saveFile.exists()) {
+            int dotIndex = saveFileName.lastIndexOf('.');
+            String baseName = (dotIndex == -1) ? saveFileName : saveFileName.substring(0, dotIndex);
+            String extension = (dotIndex == -1) ? "" : saveFileName.substring(dotIndex);
+            String newFileName = baseName + "-2" + extension;
+            saveFilePath = Paths.get(saveDirectory, newFileName).toString();
+            saveFile = new File(saveFilePath);
+        }
+        return saveFile;
+    }
+
+    /*
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        String serviceUrl = "http://localhost:5271/ProjectTools/CreateProject";
+        File[] formSchemaFiles = new File[]{
+                new File("/Users/xxx/Documents/wubuku/aptos-form-demo2/form-schema/form2.json")
+        };
+        String aptosPackageName = "AptosFormsDemo2";
+        Map<String, String> creationOptions = new HashMap<>();
+        String saveDirectory = "/Users/xxx/Downloads";
+        createContractProject(client, serviceUrl, formSchemaFiles, aptosPackageName, creationOptions, saveDirectory);
+    }
+     */
+}
---
+++ aptos-java-service/aptosformsdemo-service-rest/src/main/java/org/test/aptosformsdemo/utils/ZipUtil.java
@@ -0,0 +1,92 @@
+package org.test.aptosformsdemo.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipUtil {
+    private ZipUtil() {
+    }
+
+    public static void unzip(String zipFilePath, String destDirPath) throws IOException {
+        unzip(new File(zipFilePath), new File(destDirPath));
+    }
+
+    public static void unzip(File zipFile, File destDir) throws IOException {
+        try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zipFile.toPath()))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = newFile(destDir, zipEntry);
+                if (zipEntry.isDirectory()) {
+                    if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                        throw new IOException("Failed to create directory " + newFile);
+                    }
+                } else {
+                    // Create all parent directories
+                    File parent = newFile.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs()) {
+                        throw new IOException("Failed to create directory " + parent);
+                    }
+                    // Write the file to disk
+                    try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        }
+    }
+
+    private static File newFile(File destDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destDir, zipEntry.getName());
+
+        String destDirPath = destDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the destination directory: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+
+    // Get the extraction subdirectory path
+    public static String getExtractionSubdirectoryPath(String destDirPath, String zipFilePath) {
+        // Handle whether there is a file separator at the end
+        String destDir = destDirPath;
+        if (!destDir.endsWith(File.separator)) {
+            destDir += File.separator;
+        }
+        // Get the file name without the extension
+        String fileName = new File(zipFilePath).getName();
+        int indexOfDot = fileName.lastIndexOf('.');
+        String baseName = (indexOfDot == -1) ? fileName : fileName.substring(0, indexOfDot);
+        // If the file name does not end with .zip, use the file name as the base name
+        if (!fileName.endsWith(".zip")) {
+            baseName = fileName;
+        }
+
+        Path subDirPath = Paths.get(destDir).resolve(baseName);
+        return subDirPath.toString();
+    }
+
+
+    /*
+     public static void main(String[] args) throws IOException {
+         String zipFilePath = "/Users/xxx/Downloads/Generated_2.zip";
+         String destDirPath = "/Users/xxx/Downloads";
+         String extractionSubdirectoryPath = getExtractionSubdirectoryPath(destDirPath, zipFilePath);
+         unzip(zipFilePath, extractionSubdirectoryPath);
+     }
+     */
+}
